f1b32bf18a200d731077d6e88eddf536fb0b4ce9
Yashar Aliabbasi <yasharvc@yahoo.com>
2023-10-06 19:20:53 +0300

    Test foundations copied

---
+++ Tribitgroup.Framework/test/Tribitgroup.Framework.Identity.Tests/DbContextTest/DB/BookTemp.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tribitgroup.Framework.Identity.Tests.DbContextTest.DB
+{
+    [Table("BooksTemp")]
+    internal class BookTemp : Book
+    {
+        public bool IsTemp { get; set; } = false;
+        //public static implicit operator BookTemp(Book b)
+        //{
+        //    return new BookTemp { Name = b.Name, Id = b.Id };
+        //}
+    }
+}
---
+++ Tribitgroup.Framework/test/Tribitgroup.Framework.Identity.Tests/DbContextTest/DB/ISBN.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Tribitgroup.Framework.Shared.Types;
+
+namespace Tribitgroup.Framework.Identity.Tests.DbContextTest.DB
+{
+    internal class ISBN : Entity
+    {
+        public string Code { get; set; } = string.Empty;
+        public Guid BookId { get; set; }
+    }
+}
