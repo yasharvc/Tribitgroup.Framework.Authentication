3b55e333c1a04367a45cc9725c017a652de63375
Yashar Aliabbasi <yasharvc@yahoo.com>
2023-10-13 20:20:43 +0300

    Add UserTokenDomainService and implement CreateUserTokenAsync

---
+++ Tribitgroup.Framework/src/core/Tribitgroup.Framework.Identity.Shared/DomainServices/UserTokenDomainService.cs
@@ -0,0 +1,18 @@
+﻿using Tribitgroup.Framework.Identity.Shared.Entities;
+
+namespace Tribitgroup.Framework.Identity.Shared.DomainServices
+{
+    public sealed class UserTokenDomainService
+    {
+        public Task<UserToken> CreateUserTokenAsync(
+            Guid deviceId, 
+            UserDevice device, 
+            string token, 
+            DateTime validUntil, 
+            bool isActive)
+        {
+            var res = new UserToken(deviceId, device, token, validUntil, isActive);
+            return Task.FromResult(res);
+        }
+    }
+}
