5c6928d0d91ef3b1f91d2b60dc6cd10311b775d6
Yashar Aliabbasi <yasharvc@yahoo.com>
2023-10-14 21:56:44 +0300

    Add IUserActivityLogRepository

---
+++ Tribitgroup.Framework/src/core/Tribitgroup.Framework.Identity.Shared/Repositories/IUserActivityLogRepository.cs
@@ -0,0 +1,9 @@
+﻿using Tribitgroup.Framework.Identity.Shared.Entities;
+using Tribitgroup.Framework.Shared.Interfaces;
+
+namespace Tribitgroup.Framework.Identity.Shared.Repositories
+{
+    public interface IUserActivityLogRepository : ICUDRepository<Role, Guid>, IQueryRepository<UserActivityLog, Guid>
+    {
+    }
+}
