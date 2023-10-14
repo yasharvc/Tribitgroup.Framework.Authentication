b5afa8b10b8030f2435781b1398cddd97cdf87ee
Yashar Aliabbasi <yasharvc@yahoo.com>
2023-10-14 13:51:54 +0300

    Add corresponding properties into UserDevice

--- Tribitgroup.Framework/src/core/Tribitgroup.Framework.Identity.Shared/Entities/UserDevice.cs
+++ Tribitgroup.Framework/src/core/Tribitgroup.Framework.Identity.Shared/Entities/UserDevice.cs
@@ -1,6 +1,14 @@
-﻿namespace Tribitgroup.Framework.Identity.Shared.Entities
+﻿using Tribitgroup.Framework.Shared.Interfaces;
+using Tribitgroup.Framework.Shared.Types;
+
+namespace Tribitgroup.Framework.Identity.Shared.Entities
 {
-    public class UserDevice
+    public class UserDevice : Entity, ILogicalDelete, IHasActiveStatus
     {
+        public bool Deleted { get; set; }
+        public bool IsActive { get; set; }
+        public string IP { get; set; } = string.Empty;
+        public DateTime? ValidUntil { get; set; }
+        public string Name { get; set; } = string.Empty;
     }
 }
