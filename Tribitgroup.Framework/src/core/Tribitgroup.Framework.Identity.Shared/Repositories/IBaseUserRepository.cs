f79f91dd9d42106c6149f55e90d18b155facf192
Yashar Aliabbasi <yasharvc@yahoo.com>
2023-10-14 23:42:43 +0300

    Add guid accepting version of mehods

--- Tribitgroup.Framework/src/core/Tribitgroup.Framework.Identity.Shared/Repositories/IBaseUserRepository.cs
+++ Tribitgroup.Framework/src/core/Tribitgroup.Framework.Identity.Shared/Repositories/IBaseUserRepository.cs
@@ -15,15 +15,30 @@ namespace Tribitgroup.Framework.Identity.Shared.Repositories
         Task DeleteAsync(TUser user);
         Task ActivateAsync(TUser user);
         Task DeactivateAsync(TUser user);
+
         Task AddPermissionAsync(TUser user, params Permission[] permissions);
+        Task AddPermissionAsync(Guid userId, params Guid[] permissionIds);
         Task RemovePermissionAsync(TUser user, params Permission[] permissions);
+        Task RemovePermissionAsync(Guid userId, params Guid[] permissionIds);
+
         Task AddRoleAsync(TUser user, params Role[] roles);
+        Task AddRoleAsync(Guid userId, params Guid[] roleIds);
         Task RemoveRoleAsync(TUser user, params Role[] roles);
+        Task RemoveRoleAsync(Guid userId, params Guid[] roleIds);
+
         Task AddTenantAsync(TUser user, params Tenant[] tenants);
+        Task AddTenantAsync(Guid userId, params Guid[] tenantIds);
         Task RemoveTenantAsync(TUser user, params Tenant[] tenants);
+        Task RemoveTenantAsync(Guid userId, params Guid[] tenantIds);
+
         Task AddPolicyAsync(TUser user, params Policy[] policies);
+        Task AddPolicyAsync(Guid userId, params Guid[] policies);
+        Task RemovePolicyAsync(TUser user, params Policy[] policies);
+        Task RemovePolicyAsync(Guid userId, params Guid[] policies);
+
         Task AddTokenAsync(TUser user, Guid deviceId, string token, DateTime validUntil);
         Task DeactivateTokenAsync(Guid tokenId);
+
         Task UpdateLastLoginDateTimeAsynv(TUser user);
     }
 }
