a7549e647e66a780be0d4ab174900e2de2ff6109
Yashar Aliabbasi <yasharvc@yahoo.com>
2023-10-14 14:23:18 +0300

    Add UserActivityLog entity into Identity.Shared

--- Tribitgroup.Framework/src/core/Tribitgroup.Framework.Identity.Shared/Entities/UserActivityLog.cs
+++ Tribitgroup.Framework/src/core/Tribitgroup.Framework.Identity.Shared/Entities/UserActivityLog.cs
@@ -1,6 +1,12 @@
-﻿namespace Tribitgroup.Framework.Identity.Shared.Entities
+﻿using Tribitgroup.Framework.Identity.Shared.Enums;
+using Tribitgroup.Framework.Shared.Types;
+
+namespace Tribitgroup.Framework.Identity.Shared.Entities
 {
-    public class UserActivityLog
+    public class UserActivityLog : AuditedAggregateRoot
     {
+        public UserActivityEnum Activity { get; set; }
+        public string? Metadata { get; set; } = string.Empty;
+        public Guid UserId { get; set; }
     }
 }
