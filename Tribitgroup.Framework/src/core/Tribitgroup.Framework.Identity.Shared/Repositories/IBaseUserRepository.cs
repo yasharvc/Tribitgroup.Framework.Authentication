7e6f49fe437de5253971ef330b3b1d45661ab190
Yashar Aliabbasi <yasharvc@yahoo.com>
2023-10-14 21:59:07 +0300

    Add IBaseUserRepository

---
+++ Tribitgroup.Framework/src/core/Tribitgroup.Framework.Identity.Shared/Repositories/IBaseUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Tribitgroup.Framework.Identity.Shared.Entities;
+using Tribitgroup.Framework.Shared.Interfaces;
+
+namespace Tribitgroup.Framework.Identity.Shared.Repositories
+{
+    public interface IBaseUserRepository : ICUDRepository<BaseUser, Guid>, IQueryRepository<BaseUser, Guid>
+    {
+    }
+}
