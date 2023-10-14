7f88fcc018ae312d166359d3714d9071ef7c9897
Yashar Aliabbasi <yasharvc@yahoo.com>
2023-10-14 13:55:51 +0300

    Add tenant entity

--- Tribitgroup.Framework/src/core/Tribitgroup.Framework.Identity.Shared/Entities/Tenant.cs
+++ Tribitgroup.Framework/src/core/Tribitgroup.Framework.Identity.Shared/Entities/Tenant.cs
@@ -1,6 +1,20 @@
-﻿namespace Tribitgroup.Framework.Identity.Shared.Entities
+﻿using Tribitgroup.Framework.Shared.Types;
+
+namespace Tribitgroup.Framework.Identity.Shared.Entities
 {
-    public class Tenant
+    public class Tenant : Entity
     {
+        public string PathToParent { get; set; } = string.Empty;
+        public string ShortKey { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+
+        public Tenant(string pathToParent, string shortKey, string title)
+        {
+            PathToParent = pathToParent;
+            ShortKey = shortKey;
+            Title = title;
+        }
+
+        public Tenant(string shortKey, string title) : this("", shortKey, title) { }
     }
 }
