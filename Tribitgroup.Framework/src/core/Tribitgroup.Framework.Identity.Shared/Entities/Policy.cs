424159651336cc408aa065642c1f9f5f391597ca
Yashar Aliabbasi <yasharvc@yahoo.com>
2023-10-14 14:01:28 +0300

    Add Policy entity into Identity.Shared

--- Tribitgroup.Framework/src/core/Tribitgroup.Framework.Identity.Shared/Entities/Policy.cs
+++ Tribitgroup.Framework/src/core/Tribitgroup.Framework.Identity.Shared/Entities/Policy.cs
@@ -1,6 +1,17 @@
-﻿namespace Tribitgroup.Framework.Identity.Shared.Entities
+﻿using Tribitgroup.Framework.Shared.Types;
+
+namespace Tribitgroup.Framework.Identity.Shared.Entities
 {
-    public class Policy
+    public class Policy : Entity
     {
+        public string Name { get; set; } = string.Empty;
+        public virtual ICollection<Role> Roles { get; set; } = new HashSet<Role>();
+        public virtual ICollection<Permission> Permissions { get; set; } = new HashSet<Permission>();
+        public virtual ICollection<Tenant> Tenants { get; set; } = new HashSet<Tenant>();
+
+        public Policy(string name)
+        {
+            Name = name;
+        }
     }
 }
