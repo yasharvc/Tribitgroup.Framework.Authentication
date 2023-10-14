d219df9b131a6422fb890098d452f29be3f00926
Yashar Aliabbasi <yasharvc@yahoo.com>
2023-10-14 13:59:12 +0300

    Add Permission entity into Identity.Shared

--- Tribitgroup.Framework/src/core/Tribitgroup.Framework.Identity.Shared/Entities/Permission.cs
+++ Tribitgroup.Framework/src/core/Tribitgroup.Framework.Identity.Shared/Entities/Permission.cs
@@ -1,6 +1,14 @@
-﻿namespace Tribitgroup.Framework.Identity.Shared.Entities
+﻿using Tribitgroup.Framework.Shared.Types;
+
+namespace Tribitgroup.Framework.Identity.Shared.Entities
 {
-    public class Permission
+    public class Permission : Entity
     {
+        public string Name { get; set; } = string.Empty;
+        private Permission() { }
+        public Permission(string name)
+        {
+            Name = name;
+        }
     }
 }
