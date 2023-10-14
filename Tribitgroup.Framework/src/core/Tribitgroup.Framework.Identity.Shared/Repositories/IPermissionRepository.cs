734ca70b5ea535a27bf053c135e28e7684dbd182
Yashar Aliabbasi <yasharvc@yahoo.com>
2023-10-14 15:01:28 +0300

    Add IPermissionRepository and corrected Permission type

---
+++ Tribitgroup.Framework/src/core/Tribitgroup.Framework.Identity.Shared/Repositories/IPermissionRepository.cs
@@ -0,0 +1,9 @@
+﻿using Tribitgroup.Framework.Identity.Shared.Entities;
+using Tribitgroup.Framework.Shared.Interfaces;
+
+namespace Tribitgroup.Framework.Identity.Shared.Repositories
+{
+    public interface IPermissionRepository : ICUDRepository<Role, Guid>, IQueryRepository<Permission, Guid>
+    {
+    }
+}
