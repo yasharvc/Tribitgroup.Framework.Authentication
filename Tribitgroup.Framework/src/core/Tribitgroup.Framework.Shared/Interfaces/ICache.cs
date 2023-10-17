7d38cfe7d6b19ec27aae0dd0a0fdf6a62ca4d181
Yashar Aliabbasi <yasharvc@yahoo.com>
2023-10-17 16:29:00 +0300

    Support of null return for non exist key in cache

--- Tribitgroup.Framework/src/core/Tribitgroup.Framework.Shared/Interfaces/ICache.cs
+++ Tribitgroup.Framework/src/core/Tribitgroup.Framework.Shared/Interfaces/ICache.cs
@@ -2,14 +2,14 @@
 {
     public interface ICache<TKey, TValue>
     {
-        Task<TValue> GetAsync(TKey key);
+        Task<TValue?> GetAsync(TKey key);
         Task<IEnumerable<TValue>> GetAllAsync();
         Task AddOrUpdateAsync(TKey key, TValue value, TimeSpan? expiration = null);
         Task RemoveAsync(TKey key);
         Task<bool> ContainsKeyAsync(TKey key);
         Task ClearAsync();
         TimeSpan? AutoClearTime { get; }
-        uint MaxSizeInBytes { get; }
+        ulong MaxSizeInBytes { get; }
     }
 
     public interface IEntityCache : ICache<Guid, IEntity<Guid>> { }
