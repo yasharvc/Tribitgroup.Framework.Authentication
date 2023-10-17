f030738fa8ff565491d3fe62bc93cdbd9caf7259
Yashar Aliabbasi <yasharvc@yahoo.com>
2023-10-17 17:02:45 +0300

    Refactore RedisCache into three main classes
    BaseRedisCache do the all actions necessary for Redis part, RedisBasicTypeCache is for basic types like string, integer, long and so on, and RedisObjectCache is for objects

--- Tribitgroup.Framework/src/core/Tribitgroup.Framework.Shared/Services/RedisObjectCache.cs
+++ Tribitgroup.Framework/src/core/Tribitgroup.Framework.Shared/Services/RedisObjectCache.cs
@@ -1,77 +1,22 @@
-﻿using StackExchange.Redis;
-using Tribitgroup.Framework.Shared.Extensions;
+﻿using Tribitgroup.Framework.Shared.Extensions;
 using Tribitgroup.Framework.Shared.Interfaces;
 using Tribitgroup.Framework.Shared.Types;
 
 namespace Tribitgroup.Framework.Shared.Services
 {
-    public class RedisObjectCache<TValue> : ICache<string, TValue> where TValue : class
+    public class RedisObjectCache<TValue> : BaseRedisCache<TValue>, ICache<string, TValue> where TValue : class
     {
-        public TimeSpan? AutoClearTime { get; private set; } = TimeSpan.Zero;
-        public ulong MaxSizeInBytes { get; private set; } = 1024 * 2;
-        protected RedisSetting RedisSetting { get; private set; }
-        protected IDatabase Cache { get; private set; }
-
-        public RedisObjectCache(RedisSetting redisSetting, ulong maxSizeInBytes, TimeSpan? autoClearTime = null)
+        public RedisObjectCache(RedisSetting redisSetting, ulong maxSizeInBytes, TimeSpan? autoClearTime = null) 
+            : base(redisSetting, maxSizeInBytes, autoClearTime)
         {
-            if (autoClearTime.HasValue && autoClearTime.Value < TimeSpan.Zero)
-                throw new InvalidDataException(nameof(autoClearTime));
-
-            AutoClearTime = autoClearTime ?? TimeSpan.Zero;
-            RedisSetting = redisSetting;
-            MaxSizeInBytes = maxSizeInBytes;
-            Cache = RedisSetting.Connect();
+            if(typeof(TValue).IsBasicType())
+                throw new ArgumentException($"{typeof(TValue).FullName} should not be a basic type.");
         }
 
-        public async Task<TValue?> GetAsync(string key)
-        {
-            var json = await Cache.StringGetAsync(key);
-            return json.HasValue ? json.ToString().FromJson<TValue>() : null;
-        }
+        protected override Task<TValue> StringToValueAsync(string value) 
+            => Task.FromResult(value.FromJson<TValue>() ?? throw new InvalidCastException($"`{value}` into `{typeof(TValue).FullName}` type"));
 
-        public async Task<IEnumerable<TValue>> GetAllAsync()
-        {
-            var keyValues = new Dictionary<string, TValue>();
-            var keys = await GetKeysAsync();
-
-            foreach (var key in keys)
-                keyValues[key] = await GetAsync(key);
-
-            return keyValues.Values;
-        }
-
-        public async Task AddOrUpdateAsync(string key, TValue value, TimeSpan? expiration = null) 
-            => await Cache.StringSetAsync(key, value.ToJson(), expiration ?? AutoClearTime);
-
-        public Task RemoveAsync(string key) => Cache.StringGetDeleteAsync(key);
-
-        public Task<bool> ContainsKeyAsync(string key) 
-            => Cache.KeyExistsAsync(key);
-
-        public async Task ClearAsync()
-        {
-            var keys = await GetKeysAsync();
-            foreach(var key in keys)
-                await RemoveAsync(key);
-        }
-
-        private Task<IEnumerable<string>> GetKeysAsync()
-        {
-            long cursor = 0;
-            var keys = new List<string>();
-
-            do
-            {
-                var scanResult = Cache.Execute("SCAN", cursor.ToString());
-                var innerResult = (RedisResult[])scanResult;
-
-                cursor = long.Parse((string)innerResult[0]);
-
-                List<string> resultLines = ((string[])innerResult[1]).ToList();
-                keys.AddRange(resultLines);
-
-            } while (cursor != 0);
-            return Task.FromResult(keys.AsEnumerable());
-        }
+        protected override Task<string> ValueToStringAsync(TValue value)
+            => Task.FromResult(value.ToJson());
     }
 }
