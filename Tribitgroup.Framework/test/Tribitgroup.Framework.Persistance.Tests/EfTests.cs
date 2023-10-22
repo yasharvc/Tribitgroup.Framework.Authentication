0680f0094f6f3096709d42ce4ca41c0017be9933
Yashar Aliabbasi <yasharvc@yahoo.com>
2023-10-22 09:57:23 +0300

    Test UOW for delete

--- Tribitgroup.Framework/test/Tribitgroup.Framework.Persistance.Tests/EfTests.cs
+++ Tribitgroup.Framework/test/Tribitgroup.Framework.Persistance.Tests/EfTests.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Tribitgroup.Framework.Identity.EF.Repositories;
 using Tribitgroup.Framework.Persistance.Tests.DbContext;
+using Tribitgroup.Framework.Shared.Interfaces;
 using Tribitgroup.Framework.Test.Helper;
 
 namespace Tribitgroup.Framework.Dapper.Tests
@@ -41,6 +42,39 @@ namespace Tribitgroup.Framework.Dapper.Tests
             GetDbContext().Users.First().Password.ShouldBe(user.Password);
             GetDbContext().Users.First().Id.ShouldBe(user.Id);
             GetDbContext().Users.First().DateOfBirth.ShouldBe(user.DateOfBirth);
+        }
+
+        [Fact]
+        public async Task UOW_Should_Rollback_Updated_User()
+        {
+            User user = await AddUserAsync("User 1","Pass 1");
+            var ctx = GetDbContext();
+
+            var tran = await ctx.Database.BeginTransactionAsync();
+
+            await repository.DeleteOneAsync(user.Id, unitOfWorkHost: new UnitOfWorkHostInterface<SampleDbContext>(ctx));
+
+            ctx.Users.Count().ShouldBe(0);
+
+            await tran.RollbackAsync();
+
+            ctx.Users.Count().ShouldBe(1);
+            ctx.Users.First().Password.ShouldBe(user.Password);
+            ctx.Users.First().Id.ShouldBe(user.Id);
+            ctx.Users.First().DateOfBirth.ShouldBe(user.DateOfBirth);
+        }
+
+        private async Task<User> AddUserAsync(string username, string pass)
+        {
+            var user = new User
+            {
+                DateOfBirth = DateTime.Now.AddYears(-10),
+                Password = pass,
+                Username = username,
+            };
+
+            await repository.InsertOneAsync(user);
+            return user;
         }
     }
 }
