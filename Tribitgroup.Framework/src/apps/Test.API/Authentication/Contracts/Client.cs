68482feec27749320a149ade2453951992ce6eee
Yashar Aliabbasi <yasharvc@yahoo.com>
2024-01-31 15:31:44 +0300

    Add PolicyEvaluator Feature

--- Tribitgroup.Framework/src/apps/Test.API/Authentication/Contracts/Client.cs
+++ Tribitgroup.Framework/src/apps/Test.API/Authentication/Contracts/Client.cs
@@ -12,7 +12,7 @@ namespace Test.API.Authentication.Contracts
         public IEnumerable<Role> Roles { get; set; } = new List<Role>();
         public IEnumerable<Permission> Permissions { get; set; } = new List<Permission>();
         public IEnumerable<Tenant> Tenants { get; set; } = new List<Tenant>();
-        public IEnumerable<Policy> Policies { get; set; } = new List<Policy>();
+        public IEnumerable<Policy> Policies { get; private set; } = new List<Policy>();
         public string SessionId { get; set; } = string.Empty;
         public string ApplicationToken { get; set; } = string.Empty;
         public string TenantId { get; set; } = string.Empty;
@@ -26,11 +26,31 @@ namespace Test.API.Authentication.Contracts
         public HttpCommandEnum HttpCommand { get; set; } = HttpCommandEnum.GET;
         public string HttpScheme { get; set; } = "http";
         public string HttpProtocol { get; set; } = "HTTP/1.1";
-
         public string Subdomain { get; set; } = string.Empty;
-
         public string Domain { get; set; } = string.Empty;
 
+        public Dictionary<Policy, PolicyEvaluatorDelegate<Tenant, Policy, Role, Permission>> PolicyEvaluators { get; set; } = [];
+
         public LocalizationInfo Localization { get; set; } = new LocalizationInfo();
+
+        public void AddPolicy(Policy policy, PolicyEvaluatorDelegate<Tenant, Policy, Role, Permission> policyEvaluator)
+        {
+            var policies = Policies.ToList();
+            policies.Add(policy);
+
+            PolicyEvaluators.Add(policy, policyEvaluator);
+
+            Policies = policies;
+        }
+
+        public void Addpolicy(Policy policy, IPolicyEvaluator<Tenant, Policy, Role, Permission> policyEvaluator)
+        {
+            var policies = Policies.ToList();
+            policies.Add(policy);
+
+            PolicyEvaluators.Add(policy, policyEvaluator.EvaluateAsync);
+
+            Policies = policies;
+        }
     }
 }
--- Tribitgroup.Framework/src/apps/Test.API/Authentication/Contracts/IClient.cs
+++ Tribitgroup.Framework/src/apps/Test.API/Authentication/Contracts/IClient.cs
@@ -3,7 +3,7 @@ using Test.API.Authentication.Models;
 
 namespace Test.API.Authentication.Contracts
 {
-    public interface IClient<Tenant, Policy, Role, Permission> 
+    public interface IClient<Tenant, Policy, Role, Permission>
         where Role : IRole
         where Permission : IPermission
         where Tenant : ITenant
@@ -11,7 +11,7 @@ namespace Test.API.Authentication.Contracts
     {
         IEnumerable<Role> Roles { get; }
         IEnumerable<Permission> Permissions { get; }
-        IEnumerable<Tenant> Tenants{ get; }
+        IEnumerable<Tenant> Tenants { get; }
         IEnumerable<Policy> Policies { get; }
         string SessionId { get; }
         string ApplicationToken { get; }//Machine to machine token
@@ -29,8 +29,8 @@ namespace Test.API.Authentication.Contracts
         string Subdomain { get; }
         string Domain { get; }
         LocalizationInfo Localization { get; }
+        Dictionary<Policy, PolicyEvaluatorDelegate<Tenant, Policy, Role, Permission>> PolicyEvaluators { get; }
     }
-
     public class HttpClient<Tenant, Policy, Role, Permission> : Client<Tenant, Policy, Role, Permission>
         where Role : IRole
         where Permission : IPermission
