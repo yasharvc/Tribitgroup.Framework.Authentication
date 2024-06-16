f2d5e165d4fb5138e9d648da67e24533b54d1fc9
Yashar Aliabbasi <yasharaliabasi@gmail.com>
2024-06-16 15:54:22 +0300

    Update nuget versions

--- Tribitgroup.Framework/src/apps/Test.API/Authentication/Contracts/Role.cs
+++ Tribitgroup.Framework/src/apps/Test.API/Authentication/Contracts/Role.cs
@@ -1,7 +1,10 @@
-﻿namespace Test.API.Authentication.Contracts
+﻿
+namespace Test.API.Authentication.Contracts
 {
     public class Role : IRole
     {
         public string Name { get; set;} = string.Empty;
+
+        public Guid Id { get; set; }
     }
 }
--- Tribitgroup.Framework/src/apps/Test.API/Authentication/Contracts/Tenant.cs
+++ Tribitgroup.Framework/src/apps/Test.API/Authentication/Contracts/Tenant.cs
@@ -4,5 +4,7 @@
     {
         public Guid Id { get; set; } = Guid.Empty;
         public string Name { get; set; } = string.Empty;
+        public string Shortcut { get; set; } = string.Empty;
+        public string Path { get; set; } = string.Empty;
     }
 }
